d05d3f4257136231629890373a8406ac1b89363d
Dengheng Shi <71458749+stephannnnnie@users.noreply.github.com>
2025-03-03 10:46:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import csv
 import gridfs
 import os
 
-uri = "mongodb+srv://dengheng:LRy3rgaPUDtVd3bN@cluster1.gp2ik.mongodb.net/?retryWrites=true&w=majority&appName=cluster1"
+uri = "youruri"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
